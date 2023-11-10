f991069cf55f14bd035adf3ae8100135b5ec3bcf
Sarah Quinney <quinney.sarah@gmail.com>
2023-11-10 09:20:15 -0600

    fixed header spacing

---
+++ frontend-user-view/src/HoursLocationPage.tsx
@@ -0,0 +1,9 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
+
+export default function HoursLocationPage() {
+    return {
+        
+    }
+}
