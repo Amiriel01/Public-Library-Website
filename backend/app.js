31f8f3b5d66ace29045de6f6a8727e655352ac5e
Sarah Quinney <quinney.sarah@gmail.com>
2023-11-11 13:08:31 -0600

    updated tab name, fixed homepage link spacing

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,31 @@
+require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-import 'dotenv/config'
+const cors = require('cors')
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.SECRET_KEY;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
