1e7431e68614d15c4e68870ad4d124b37cac3fdf
Sarah Quinney <quinney.sarah@gmail.com>
2023-11-14 21:59:29 -0600

    updated tab icon

--- backend/src/controllers/contactController.ts
+++ backend/src/controllers/contactController.ts
@@ -139,8 +139,7 @@ export function contact_form_submission_edit() {
 export function contact_form_submission_delete() {
     return asyncHandler(async (req, res, next) => {
         const contactFormSubmissionDelete = await Contact.findByIdAndRemove(req.params.id).exec();
-        console.log(contactFormSubmissionDelete);
-        console.log(req.params.id)
+        console.log("item deleted")
         res.json(contactFormSubmissionDelete);
     })
 }
