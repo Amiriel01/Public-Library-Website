db5f18b085ff746f1a0c9fbefadf5e4c3e22685f
Sarah Quinney <quinney.sarah@gmail.com>
2023-11-18 17:11:53 -0600

    GivingInfo page created

--- frontend-user-view/src/GivingInfo.tsx
+++ frontend-user-view/src/GivingInfo.tsx
@@ -86,56 +86,75 @@ export default function GivingInfo() {
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>What are the library card age requirements?</Accordion.Header>
+                    <Accordion.Header>Make a Legacy</Accordion.Header>
                     <Accordion.Body>
                         <p>
-                            All ages are eligible for a library card.
+                            A legacy gift will keep giving for many generations. By making a legacy gift, you will help ensure that future generations have access to all the programs and more that are enjoyed today at the library.
                         </p>
                         <p>
-                            Minor dependents must have a parent/guardian to sponsor the minor account.
+                            While the Foundation is not able to give financial or tax advice, we are here to help. You are welcome to call the Foundation office and we will work with your financial advisors to help you carry out your vision for the future.
                         </p>
+                        <h4 id='ways-to-give-header'>How the library benefits from your legacy gift:</h4>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'> Planning:</p>
+                            <p>Legacy gift lets us know that resources are in the pipeline to help fund the future of the Library.</p>
+                        </div>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'>Investments:</p>
+                            <p>Legacy gifts allow the library to grow its investments to ensure the Library has adequate funding for the future.</p>
+                        </div>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'>Special Projects:</p>
+                            <p>Legacy gifts can provide the funding to embark on a special project or purchase equipment/technology that could not otherwise be afforded.</p>
+                        </div>
+                        <h4 id='ways-to-give-header'>How you benefit from your legacy gift:</h4>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'> Tax Reduction:</p>
+                            <p>Gift, estate, and capital gain taxes could be reduced or eliminated with your legacy gift.</p>
+                        </div>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header>What is my library PIN number?</Accordion.Header>
+                    <Accordion.Header>Make a Monthly Gift</Accordion.Header>
                     <Accordion.Body>
-                        <p>Unless you have specifically asked to change it, your PIN is your six-digit birthdate (two digit month, date, and year)</p>
-                        <p>Please contact the library for PIN assistance if your PIN is not working.</p>
+                        <p>If you would like to make a monthly donation, please make the selection on the donation page. Reoccuring donations can be cancelled at any time.</p>
+                        <h4 id='ways-to-give-header'>Benefits of Donating Monthly:</h4>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'> Convenience:</p>
+                            <p>You do not have to worry about remembering to go online and donate, or to mail a gift each year.</p>
+                        </div>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'> Reliability:</p>
+                            <p>The library depends on donations to support many of the offered programs. With monthly donations, the library can depend on the funds being available when needed.</p>
+                        </div>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'>Updates:</p>
+                            <p>As a monthly donator, you will recieve emails throughout the year about exibits and events. Monthly donators will have access to these events and exibits before the general public.</p>
+                        </div>
+                        <div id='bold-flex-container'>
+                            <p id='bold-text'>Baskets:</p>
+                            <p>As a special thank you, the library will send a gift basket semi-annual to all monthly donators. Expect to receive your basket every June and December. To qualify for the semi-annual basket, you must have contributed monthly for the previous five months with no interuptions.</p>
+                        </div>
+                        <p id='bold-text'>Click Donate to set up your monthly donation!</p>
+                        <Link to="/Donate">
+                            <MyButton id='donate-button' title='&#10084; Donate'></MyButton>
+                        </Link>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
-                    <Accordion.Header>Can I get a library card if I live outside the city?</Accordion.Header>
+                    <Accordion.Header>Become a Program Sponsor</Accordion.Header>
                     <Accordion.Body>
-                        <p>Non-residents library cards are available for an annual fee of $25.00 per family residence.</p>
-                        <p>A family residence includes up to two adults and any dependents under the age of eighteen in the household.</p>
-                        <p>The non-residence fee will be waived if the non-resident is a current city college student or attend one of the local public schools in the city.</p>
+                        <p>We welcome you to support our programming efforts by becoming a program sponsor. Program sponsorship is a fulfilling way to show your support of literacy and the quality Library programming available to our community members.</p>
+                        <p>We rely on you as a program sponsor to help fund current planned programs that enrich the lives of our community members. We show our appreciation of program sponsors by acknowledging them in the marketing of the sponsored program and during the sponsored program when possible.</p>
+                        <p id='bold-text'>For available sponsorship opportunities, visit the Information Desk at the library or call (555) 555-5555.</p>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="4">
-                    <Accordion.Header>Can temporary residents receive a library card?</Accordion.Header>
+                    <Accordion.Header>Adopt a Magazine</Accordion.Header>
                     <Accordion.Body>
-                        <p>Temporary residents may be issued a temporary card with a deposit of $25.00. The deposit is refunded when the card and items borrowed are returned in good condition.</p>
-                        <p>Temporary card holders are limited to two items borrowed at a time.</p>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="5">
-                    <Accordion.Header>Does my library card expire?</Accordion.Header>
-                    <Accordion.Body>
-                        <p>Library cards expire annually. If your card has expired, please contact the library staff. Once your contact information is verified, your card will be renewed.</p>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="6">
-                    <Accordion.Header>What should I do if my card is lost or damaged?</Accordion.Header>
-                    <Accordion.Body>
-                        <p>Contact the library staff if you have lost your card so the account can be put on hold. This will ensure no one else can use your card.</p>
-                        <p>Lost or damaged cards can be replaced by visiting the front desk at the library. Replacement cards cost $2.00 each.</p>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="7">
-                    <Accordion.Header>How can I get notifications about my library card account?</Accordion.Header>
-                    <Accordion.Body>
-                        <p>Library card holders will specify their contact method of choice, text or email, when they receive their library card.</p>
-                        <p>If you'd like to change your notification preference please contact the library.</p>
+                        <p>Help expand our magazine collection by donating to Adopt a Magazine. You could proudly provide the subscription fee for one or more of the magazines the Library subscribes to.</p>
+                        <p>When you adopt a magazine, a label acknowledging your adoption is placed on the shelf where the adopted magazine is housed. (Gifts may also be designated as anonymous.) Most subscriptions range in price from $15 to $30 per year.</p>
+                        <p id='bold-text'>To adopt a magazine, visit the Information Desk at the library or call (555) 555-5555.</p>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
