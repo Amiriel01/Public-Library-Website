660de9aca1476cfaf19c87bcad0b26a9286a4efd
Sarah Quinney <quinney.sarah@gmail.com>
2023-11-20 10:21:22 -0600

    volunteer schema completed

--- backend/src/models/volunteer.ts
+++ backend/src/models/volunteer.ts
@@ -3,5 +3,68 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const VolunteerSchema = new Schema({
-    
-})
+    name: {
+        type: String,
+        required: true,
+    },
+    phone: { 
+        type: String,
+        required: true,
+    }, 
+    email: {
+        type: String,
+        match: /.+\@.+\..+/,
+        required: true,
+    },
+    work_experience: {
+        type: String,
+        required: true,
+    },
+    volunteer_experience: {
+        type: String,
+        required: true,
+    },
+    monday: {
+        type: Boolean,
+    },
+    tuesday: {
+        type: Boolean,
+    },
+    wednesday: {
+        type: Boolean,
+    },
+    thursday: {
+        type: Boolean,
+    },
+    friday: {
+        type: Boolean,
+    },
+    saturday: {
+        type: Boolean,
+    },
+    sunday: {
+        type: Boolean,
+    },
+    morning: {
+        type: Boolean,
+    },
+    afternoon: {
+        type: Boolean,
+    },
+    evening: {
+        type: Boolean,
+    },
+    interviewed: {
+        type: Boolean,
+        default: false,
+    },
+    interview_notes: {
+        type: String,
+    },
+    approved: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+export default mongoose.model("Volunteer", VolunteerSchema);
