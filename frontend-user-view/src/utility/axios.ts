07fccf152f5fb9b3d8bc2925e42a902765cde201
Sarah Quinney <quinney.sarah@gmail.com>
2024-02-12 16:32:46 -0600

    deleted commented item

--- frontend-user-view/src/utility/axios.ts
+++ frontend-user-view/src/utility/axios.ts
@@ -5,8 +5,4 @@ let customAxios = axios.create({
     baseURL: import.meta.env.DEV ? "http://localhost:3000/" : "oeoewvoewvbowevb"
 });
 
-// export function updateWithKey(key: string) {
-//     customAxios.defaults.headers.common.Authorization = `Bearer ${key}`;
-// }
-
 export default customAxios;
