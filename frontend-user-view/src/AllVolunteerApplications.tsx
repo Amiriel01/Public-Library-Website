952c75d9cfc16c1567d1e993746de95c1333976f
Sarah Quinney <quinney.sarah@gmail.com>
2024-02-18 07:22:20 -0600

    update delete rendering

--- frontend-user-view/src/AllVolunteerApplications.tsx
+++ frontend-user-view/src/AllVolunteerApplications.tsx
@@ -30,7 +30,7 @@ export default function AllVolunteerApplications() {
 
     useEffect(() => {
         getVolunteerApplications();
-    }, []);
+    }, [volunteerApplicationsList]);
 
     useEffect(() => {
         window.scrollTo(0, 0)
